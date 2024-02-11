86474ae78606c7f704a8b1a817f210acaadcb9a4
Rikin07 <110732947+Senyadt2@users.noreply.github.com>
2024-02-12 00:36:01 +0545

    update

--- MernToDo/Server/src/index.js
+++ MernToDo/Server/src/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const cors = require("cors");
 
-const TodoModel = require("./Models/Todo");
+const TodoModel = require("../Models/Todo");
 
 const app = express();
 
